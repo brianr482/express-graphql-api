dc3d4b2870ccfc9df3e6a4aac415bb3a6681d7fc
Brian Ramirez <brians@uninorte.edu.co>
2021-06-01 10:59:08 -0500

    chore: Setup for JS Shouts

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
 const functions = require('firebase-functions');
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const {
-  resolvers,
-  schemas,
-  contextHandler
-} = require('./graphql');
 
 // Populate datasources with mock data
 require('./scripts/mock-data');
 
 const app = express();
 
-// Create the GraphQL server instance
-const apolloServer = new ApolloServer({
-  typeDefs: schemas,
-  resolvers,
-  context: contextHandler,
-
-  // Graphiql
-  introspection: true,
-  playground: true
-});
-
-// Integrate GraphQL server with Express
-apolloServer.applyMiddleware({ app, path: '/', cors: true });
+app.get('/', (req, res ) => res.send('Hello world!'));
 
 exports.graphql = functions.https.onRequest(app);
